8dd37447d445d48db257f6dc2e56923acfe76afc
Worik McShane <wmcshane@geometryit.com>
2023-09-26 14:59:26 +1000

    Released version 1.1.16.GEO-SNAPSHOT

--- src/main/java/com/github/jhonnymertz/wkhtmltopdf/wrapper/exceptions/WrapperConfigException.java
+++
@@ -1,9 +0,0 @@
-package com.github.jhonnymertz.wkhtmltopdf.wrapper.exceptions;
-
-public class WrapperConfigException extends RuntimeException {
-
-    public WrapperConfigException(String s, Throwable t) {
-        super(s, t);
-    }
-
-}
